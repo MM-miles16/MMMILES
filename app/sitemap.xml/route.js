f1bcff6dddcfd7b717178c6e4f61c6599b9a591e
harisha <harsha2002jc@gmail.com>
2026-01-07 17:09:29 +0530

    sitemap.xml file added for seo

---
+++ app/sitemap.xml/route.js
@@ -0,0 +1,35 @@
+export async function GET() {
+  const baseUrl = "https://www.mmmiles.com";
+
+  const pages = [
+    "",
+    "/about",
+    "/contact",
+    "/reviews",
+    "/faq",
+    "/login",
+    "/cars",
+  ];
+
+  const urls = pages
+    .map(
+      (page) => `
+  <url>
+    <loc>${baseUrl}${page}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`
+    )
+    .join("");
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
